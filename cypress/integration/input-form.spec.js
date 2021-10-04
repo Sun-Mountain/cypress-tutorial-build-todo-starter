866f856119acb7451e3236157a478fb2d9400fa7
Sun-Mountain <nzonnenberg@fearless.tech>
2021-10-04 14:16:24 -0400

    refactor test

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -18,9 +18,12 @@ describe('Input form', () => {
   })
 
   context('Form submission', () => {
+    beforeEach(() => {
+      cy.server()
+    })
+
     it('Adds a new todo on submit', () => {
       const itemText = 'Wake Up'
-      cy.server()
       cy.route('POST', '/api/todos', {
         name: itemText,
         id: 1,
@@ -34,6 +37,24 @@ describe('Input form', () => {
       cy.get('.todo-list li')
         .should('have.length', 1)
         .and('contain', itemText)
+    })
+
+    it('Shows an error message on a failed submission', () => {
+      cy.route({
+        url: '/api/todos',
+        method: 'POST',
+        status: 500,
+        response: {}
+      })
+
+      cy.get('.new-todo')
+        .type('test{enter}')
+
+      cy.get('.todo-list li')
+        .should('not.exist')
+
+      cy.get('.error')
+        .should('be.visible')
     })
   })
 })
