aba4218d6c34143c4c117a4ed4cf137d54107c3d
Sun-Mountain <nzonnenberg@fearless.tech>
2021-10-05 13:50:07 -0400

    delete todos smoke test

--- cypress/integration/smoke-tests.spec.js
+++ cypress/integration/smoke-tests.spec.js
@@ -6,7 +6,7 @@ describe('smoke tests', () => {
   })
 
   context('with no todos', () => {
-    it.only('saves new todos', () => {
+    it('saves new todos', () => {
       const items = [
         {text: 'Wake up', expectedLength: 1},
         {text: 'Feed cats', expectedLength: 2},
@@ -29,7 +29,22 @@ describe('smoke tests', () => {
           cy.get('.todo-list li')
             .should('have.length', todo.expectedLength)
         })
+    })
+  })
 
+  context('with active todos', () => {
+    beforeEach(() => {
+      cy.fixture('todos')
+        .each(todo => {
+          const newTodo = Cypress._.merge(todo, {isComplete: false})
+          cy.request('POST', '/api/todos', newTodo)
+        })
+      cy.visit('/')
+    })
+
+    it.only('loads existing data from the db', () => {
+      cy.get('.todo-list li')
+        .should('have.length', 4)
     })
   })
 })
