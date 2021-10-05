ad1536fd259b862ad259941a5dd0ef5ac1d148ce
Sun-Mountain <nzonnenberg@fearless.tech>
2021-10-05 11:41:41 -0400

    Filter active test

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -1,9 +1,28 @@
 describe('Footer', () => {
   context('with a single todo', () => {
-    it.only('displays a singular todo in count', () => {
+    it('displays a singular todo in count', () => {
       cy.seedAndVisit([{id: 1, name: 'Wake Up', isComplete: false}])
       cy.get('.todo-count')
         .should('contain', '1 todo left')
+    })
+  })
+
+  context('with multiple todos', () => {
+    beforeEach(() => {
+      cy.seedAndVisit()
+    })
+
+    it('displays plural todos in count', () => {
+      cy.get('.todo-count')
+        .should('contain', '3 todos left')
+    })
+
+    it.only('filters active todos', () => {
+      cy.contains('Active')
+        .click()
+
+      cy.get('.todo-list li')
+        .should('have.length', 3)
     })
   })
 })
