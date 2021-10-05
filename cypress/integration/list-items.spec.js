1ff1f5989dc52faf76c3e4811ed0b4bc7e49d152
Sun-Mountain <nzonnenberg@fearless.tech>
2021-10-05 09:39:21 -0400

    Count todos left

---
+++ cypress/integration/list-items.spec.js
@@ -0,0 +1,19 @@
+describe('List items', () => {
+  beforeEach(() => {
+    cy.seedAndVisit()
+  })
+
+  it('properly displays completed items', () => {
+    cy.get('.todo-list li')
+      .filter('.completed')
+      .should('have.length', 1)
+      .and('contain', 'Cats')
+      .find('.toggle')
+      .should('be.checked')
+  })
+
+  it.only('shows remaining todos in the footer', () => {
+    cy.get('.todo-count')
+      .should('contain', 3)
+  })
+})
