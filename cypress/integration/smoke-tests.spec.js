da5cd4a177cec754575a7b6db9cc007e2a4b2862
Sun-Mountain <nzonnenberg@fearless.tech>
2021-10-05 12:59:55 -0400

    create multiple todos in smoke test

--- cypress/integration/smoke-tests.spec.js
+++ cypress/integration/smoke-tests.spec.js
@@ -7,18 +7,29 @@ describe('smoke tests', () => {
 
   context('with no todos', () => {
     it.only('saves new todos', () => {
+      const items = [
+        {text: 'Wake up', expectedLength: 1},
+        {text: 'Feed cats', expectedLength: 2},
+        {text: 'Eat breakfast', expectedLength: 3}
+      ]
+
       cy.visit('/')
       cy.server()
       cy.route('POST', '/api/todos')
         .as('create')
 
-      cy.focused()
-        .type('Wake up{enter}')
+      cy.wrap(items)
+        .each(todo => {
+          cy.focused()
+            .type(todo.text)
+            .type('{enter}')
 
-      cy.wait('@create')
+          cy.wait('@create')
 
-      cy.get('.todo-list li')
-        .should('have.length', 1)
+          cy.get('.todo-list li')
+            .should('have.length', todo.expectedLength)
+        })
+
     })
   })
 })
